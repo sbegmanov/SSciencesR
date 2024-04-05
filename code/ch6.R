afb20288d324f1d81a923a16c5089b01589a41c1
sbegmanov <sbegmanov@gmail.com>
2024-04-05 22:38:05 +0500

    ch6

--- code/ch6.R
+++ code/ch6.R
@@ -115,7 +115,109 @@ rlab <- ggplot(final.85v2, aes(x = loglab, y = .resid)) +
 grid.arrange(ropen, rland, rlab, ncol = 3)
 
 # regression specification error test expanded model
-model1.q1 <- lm(logy ~ open + loglab + logland + I(.fittedˆ2), data = final.85v2)
+model1.q1 <- lm(logy ~ open + loglab + logland + I(.fitted^2), data = final.85v2)
+
+# F test of model difference
+anova(model1, model1.q1)
+
+
+# estimate a curvilinear model for trade openness
+model1.q2 <- lm(logy ~ open + loglab + logland + I(open^2), data = final.85v2)
+
+# F test of model difference
+anova(model1, model1.q2)
+summary(model1.q2)
+
+## Perfect and high multicollinearity
+
+# a variable perfectly correlating with open
+final.85v2$open4 <- 4 * final.85v2$open
+
+# correlation between two variables
+cor(final.85v2$open, final.85v2$open4, use = "complete.obs")
+
+# estimate model1 adding open4 in two different orderings OLS model
+lm(logy ~ open + open4 + loglab + logland, data = final.85v2)
+
+
+# VIF stat, VIF value of 10 is a sign of serious multicollinearity
+library(car)
+
+vif(model1)
+
+
+## Constant ERROR variance
+# non-constant error variance = heteroskedastic error
+# test heteroskedasticity, estimate OLS model and create output object
+
+model1 <- lm(logy ~ open + loglab + logland, data = final.85v2)
+
+# Cook/Weisberg score test of constant error variance
+ncvTest(model1)
+
+# Breush/Pagan test of constant error variance
+library(lmtest)
+library(sandwich)
+
+bptest(model1)
+
+# weighted least squares
+# corrections for non-constant error variance
+model1.wls <- lm(logy ~ open+loglab+logland, weights = 1 / open, data = final.85v2)
+summary(model1.wls)
+
+# report default HC3 robust standard errors
+model1.hc3 <- coeftest(model1, vcov = vcovHC)
+model1.hc3
+
+# report HC1 robust standard errors as Stata
+# variants:"HC3","HC","HC0","HC1","HC2","HC4","HC4m","HC5"
+model1.hc1 <- coeftest(model1, vcov = vcovHC(model1, type = "HC1"))
+model1.hc1
+
+# request the variance-covariance matrix HC3
+vcovHC(model1, type = "HC3")
+
+# request the variance-covariance matrix HC1
+vcovHC(model1, type = "HC1")
+
+
+## Independence of ERROR Term Observations
+
+# distribution of residuals in each region
+ggplot(final.85v2, aes(.fitted, .resid)) +
+  geom_hline(yintercept = 0) +
+  geom_point() +
+  facet_wrap(~continent)
+
+# scatter plot of trade and income by region
+ggplot(final.85, aes(open, logy)) +
+  geom_point() +
+  facet_wrap( ~ continent) +
+  stat_smooth(method = "lm", se = FALSE)
+
+# show attributes
+attributes(final.85v2$continent)
+
+# change value labels of factor variable continent
+levels(final.85v2$continent) <- c(".Africa", ".C.N.America",
+                                  ".S.America", ".Asia", ".Europe", ".Oceania")
+# model with all regional dummies and no intercept
+lm(logy ~ 0 + continent + open + loglab + logland,  data = final.85v2)
+
+# display model with all regional dummies but one
+# one region will be automatically excluded due to perfect multicollinearity
+lm(logy ~ continent + open + loglab + logland, data = final.85v2)
+
+
+
+
+
+
+
+
+
+
 
 
 
